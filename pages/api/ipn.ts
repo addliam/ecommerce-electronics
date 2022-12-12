f6422dd02b79b9ab006c6ed5c633275e76e7e090
addliam <liamyostin2004@gmail.com>
2022-12-11 20:26:57 -0500

    ipn api beta nextjs

--- pages/api/ipn.ts
+++ pages/api/ipn.ts
@@ -1,10 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
-  console.log({ req });
-  console.log({ res });
-  res.status(200).json({ res });
+import type { NextRequest } from "next/server";
+export const config = {
+  runtime: "experimental-edge",
+};
+// https://ecommerce-electronics-six.vercel.app/api/ipn?topic=payment&id=123456
+export default async function handler(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const topic = searchParams.get("topic");
+  const id = searchParams.get("id");
+  console.log({ topic, id });
+  return new Response(
+    JSON.stringify({
+      name: "working IPN",
+    }),
+    {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+        "cache-control": "public, s-maxage=1200, stale-while-revalidate=600",
+      },
+    }
+  );
 }
